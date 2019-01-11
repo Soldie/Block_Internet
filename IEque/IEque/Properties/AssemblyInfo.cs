bee4a880255bfb6bd2a358f81ad5381c4d4a8e06
Antlion <40786755+r3xq1@users.noreply.github.com>
2019-01-12 01:19:14 +0300

    Add files via upload

---
+++ IEque/IEque/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IEque")]
+[assembly: AssemblyProduct("IEque")]
+[assembly: AssemblyCopyright("Copyright © Antlion 2019")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5e363cbb-a652-4a37-b2da-981d39d907e3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
